d2398ea0b06fddf76f4d7b2ecd347746183746de
akashkumarnayak2004 <akashkumarn422@gmail.com>
2025-03-23 13:23:21 +0530

    succesfully

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
-import path from 'path';
+
 
 import cookieParser from 'cookie-parser';
 
